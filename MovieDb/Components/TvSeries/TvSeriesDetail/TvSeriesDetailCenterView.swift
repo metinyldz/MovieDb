8ff990a23dd1b00e645b5be86814eb82f1f79c0e
metinyldz <metinyildiz2006@gmail.com>
2022-12-19 23:04:01 +0300

    Fixed error

--- MovieDb/Components/TvSeries/TvSeriesDetail/TvSeriesDetailCenterView.swift
+++ MovieDb/Components/TvSeries/TvSeriesDetail/TvSeriesDetailCenterView.swift
@@ -13,52 +13,53 @@ struct TvSeriesDetailCenterView: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            RatingView(rating: "\(content.vote_average ?? -1)")
-            
-            Text(content.original_name ?? "-")
-                .font(Font.system(size: 28))
-                .fontWeight(.bold)
-                .foregroundColor(.black)
-            
-            Text(Converter.genreText(genres: content.genres!))
-                .font(Font.system(size: 15))
-                .fontWeight(.medium)
-                .foregroundColor(.black)
-
-            HStack {
-                Image("time")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 15, height: 15)
+            if let genres = content.genres {
+                RatingView(rating: "\(content.vote_average ?? -1)")
                 
-                Text("\(content.episode_run_time?.first ?? 0) min")
-                    .font(Font.system(size: 15))
-                    .fontWeight(.regular)
+                Text(content.original_name ?? "-")
+                    .font(Font.system(size: 28))
+                    .fontWeight(.bold)
                     .foregroundColor(.black)
-                    .opacity(0.6)
+                
+                Text(Converter.genreText(genres: genres))
+                    .font(Font.system(size: 15))
+                    .fontWeight(.medium)
+                    .foregroundColor(.black)
+
+                HStack {
+                    Image("time")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 15, height: 15)
+                    
+                    Text("\(content.episode_run_time?.first ?? 0) min")
+                        .font(Font.system(size: 15))
+                        .fontWeight(.regular)
+                        .foregroundColor(.black)
+                        .opacity(0.6)
+                    
+                    Divider()
+                        .padding(.horizontal, 10)
+                        .frame(height: 12)
+                    
+                    Image("calendar")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 15, height: 15)
+                    
+                    Text("TV Series (\(Converter.convertDate(input: content.first_air_date ?? "-", dateType: .year)) - \(Converter.convertDate(input: content.last_air_date ?? "-", dateType: .year)))")
+                        .font(Font.system(size: 15))
+                        .fontWeight(.regular)
+                        .foregroundColor(.black)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.6)
+                        .opacity(0.6)
+                    
+                } //: HStack
                 
                 Divider()
-                    .padding(.horizontal, 10)
-                    .frame(height: 12)
-                
-                Image("calendar")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 15, height: 15)
-                
-                Text("TV Series (\(Converter.convertDate(input: content.first_air_date ?? "-", dateType: .year)) - \(Converter.convertDate(input: content.last_air_date ?? "-", dateType: .year)))")
-                    .font(Font.system(size: 15))
-                    .fontWeight(.regular)
-                    .foregroundColor(.black)
-                    .lineLimit(1)
-                    .minimumScaleFactor(0.6)
-                    .opacity(0.6)
-                
-            } //: HStack
-            
-            Divider()
-                .padding(.vertical, 20)
-            
+                    .padding(.vertical, 20)
+            }
         } //: VStack
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding(.horizontal, 24)
