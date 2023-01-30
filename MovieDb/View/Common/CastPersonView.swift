409ac5dd70eaa27fd2a431de0265423fad9e0a95
metinyldz <metinyildiz2006@gmail.com>
2023-01-30 19:36:09 +0300

    Fixed CastPersonView

--- MovieDb/View/Common/CastPersonView.swift
+++ MovieDb/View/Common/CastPersonView.swift
@@ -10,93 +10,109 @@ import SwiftUI
 struct CastPersonView: View {
     
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-    
     var castPeople: CastPeopleModel?
     
     var body: some View {
         ScrollView {
             if let castPeople = castPeople {
-                //MARK: - HEADER -
-                
                 VStack {
-                    ZStack {
-                        AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500\(castPeople.profile_path ?? "")")) { image in
-                            image
-                                .resizable()
-                                .scaledToFill()
-                                .frame(maxWidth: .infinity, maxHeight: 400)
-                                .clipped()
-                                //.padding(.top, -50)
-                        } placeholder: {
-                            Image("moviePlaceholder")
-                                .resizable()
-                                .scaledToFill()
-                                .frame(maxWidth: .infinity, maxHeight: 400)
-                                .clipped()
-                                //.padding(.top, -50)
-                        }
-                        
-                        VStack {
-                            HStack {
-                                Image(systemName: "arrow.left")
-                                    .foregroundColor(Color.white)
-                                    .frame(width: 25, height: 25)
-                                    .onTapGesture {
-                                        self.presentationMode.wrappedValue.dismiss()
-                                    }
-                                    .padding([.leading, .trailing], 24)
-                                
-                                Spacer()
-                            } //: HStack
-                            .frame(maxWidth: .infinity, maxHeight: 50)
-                            
-                            Spacer()
-                        } //: VStack
-                    } //: ZStack
+                    //MARK: - HEADER -
+                    CastPersonHeaderView(presentationMode: _presentationMode, castPeople: castPeople)
                     
                     //MARK: - CENTER -
-                    
-                    Text(castPeople.name ?? "")
-                        .fontWeight(.bold)
-                        .font(.system(size: 28))
-                        .frame(height: 34)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.horizontal, 24)
-                        .padding(.top, 20)
-                    
-                    Text(castPeople.biography ?? "-")
-                        .fontWeight(.regular)
-                        .font(.system(size: 17))
-                        .opacity(0.8)
-                        .foregroundColor(.black)
-                        .padding(.horizontal, 24)
+                    CastPersonCenterView(castPeople: castPeople)
                     
                     //MARK: - FOOTER -
                     
-                    HStack {
-                        Text("Born: ")
-                            .fontWeight(.bold)
-                            .font(.system(size: 17))
-                            .opacity(0.8)
-                            .foregroundColor(.black)
-                        
-                        Text(castPeople.place_of_birth ?? "-")
-                            .fontWeight(.regular)
-                            .font(.system(size: 17))
-                            .opacity(0.8)
-                            .foregroundColor(.black)
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: 24, alignment: .leading)
-                    .padding(.horizontal, 24)
+                    CastPersonFooterView(castPeople: castPeople)
                 } //: VStack
-                .padding(.bottom, 10)
-                
             }
         } //: SCROLL
         .background(Color("BackgroundColor"))
         .navigationBarHidden(true)
-        //.ignoresSafeArea(.all)
-        // TODO: - Fix the top of the header issue
+    }
+}
+
+struct CastPersonHeaderView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    var castPeople: CastPeopleModel?
+    
+    var body: some View {
+        ZStack {
+            AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500\(castPeople?.profile_path ?? "")")) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(maxWidth: .infinity, maxHeight: 400)
+                    .clipped()
+            } placeholder: {
+                Image("moviePlaceholder")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(maxWidth: .infinity, maxHeight: 400)
+                    .clipped()
+            }
+            
+            VStack {
+                HStack {
+                    Image(systemName: "arrow.left")
+                        .foregroundColor(Color.white)
+                        .frame(width: 25, height: 25)
+                        .onTapGesture {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }
+                        .padding([.leading, .trailing], 24)
+                    
+                    Spacer()
+                } //: HStack
+                .frame(maxWidth: .infinity, maxHeight: 50)
+                
+                Spacer()
+            } //: VStack
+        } //: ZStack
+    }
+}
+
+struct CastPersonCenterView: View {
+    var castPeople: CastPeopleModel?
+    
+    var body: some View {
+        Text(castPeople?.name ?? "")
+            .fontWeight(.bold)
+            .font(.system(size: 28))
+            .frame(height: 34)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, 24)
+            .padding(.top, 20)
+        
+        Text(castPeople?.biography ?? "-")
+            .fontWeight(.regular)
+            .font(.system(size: 17))
+            .opacity(0.8)
+            .foregroundColor(.black)
+            .padding(.horizontal, 24)
+    }
+}
+
+struct CastPersonFooterView: View {
+    var castPeople: CastPeopleModel?
+    
+    var body: some View {
+        HStack {
+            Text("Born: ")
+                .fontWeight(.bold)
+                .font(.system(size: 17))
+                .opacity(0.8)
+                .foregroundColor(.black)
+            
+            Text(castPeople?.place_of_birth ?? "-")
+                .fontWeight(.regular)
+                .font(.system(size: 17))
+                .opacity(0.8)
+                .foregroundColor(.black)
+        }
+        .frame(maxWidth: .infinity, maxHeight: 24, alignment: .leading)
+        .padding(.horizontal, 24)
     }
 }
 
