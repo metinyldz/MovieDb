5e9ec39cabd1fce6b28e8c6ac3dbc4c7952ba169
metinyldz <metinyildiz2006@gmail.com>
2023-01-30 20:31:04 +0300

    Edited MovieView

--- MovieDb/View/Movie/MovieView.swift
+++ MovieDb/View/Movie/MovieView.swift
@@ -32,7 +32,6 @@ struct MovieView: View {
                         MovieDescriptionView(rating: results[contentBindigs.moviePageIndex].vote_average,
                                              movie: results[contentBindigs.moviePageIndex],
                                              genres: genres)
-                        .padding(.horizontal, 24)
                         
                         Divider()
                             .padding(.horizontal, 24)
@@ -40,37 +39,48 @@ struct MovieView: View {
                         
                         //MARK: - FOOTER -
                         
-                        VStack {
-                            Text("Popular")
-                                .font(Font.system(size: 22))
-                                .fontWeight(.bold)
-                        } //: VSTACK
-                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-                        .padding(.horizontal, 24)
-                        .padding(.bottom, 10)
-                        
-                        ForEach(moviesResults, id: \.self) { movie in
-                            NavigationLink(destination: MovieDetailView(content: movieViewModel.movieDetail), isActive: $movieViewModel.isMovieDetailActive) {
-                                PopularCardView(movieResult: movie, isFavorite: getFavoriteItem(movie), genres: genres)
-                                    .padding(.vertical, 10)
-                                    .environmentObject(contentBindigs)
-                                    .onTapGesture {
-                                        movieViewModel.getMovieDetail(id: movie.id ?? -1)
-                                    }
-                            } //: LINK
-                        } //: LOOP
+                        MovieFooterView(moviesResults: moviesResults, genres: genres)
                     }
                 } //: SCROLL
-                .navigationBarTitle("Movies", displayMode: .large)
-                .navigationBarColor(backgroundColor: Color("VibrantBlue"), titleColor: .white)
             } //: ZStack
+            .navigationBarTitle("Movies", displayMode: .large)
             .navigationViewStyle(.stack)
+            .navigationBarColor(backgroundColor: Color("VibrantBlue"), titleColor: .white)
         } //: NAVIGATION
         .onAppear {
             movieViewModel.getTopRatedMovies()
             movieViewModel.getMovies()
             movieViewModel.getMoviesGenres()
         }
+    }
+}
+
+struct MovieFooterView: View {
+    @StateObject var movieViewModel = MovieViewModel()
+    @EnvironmentObject var contentBindigs: ContentBindigs
+    var moviesResults: [MovieResult]
+    var genres: [GenreResult]
+    
+    var body: some View {
+        VStack {
+            Text("Popular")
+                .font(Font.system(size: 22))
+                .fontWeight(.bold)
+        } //: VSTACK
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+        .padding(.horizontal, 24)
+        .padding(.bottom, 10)
+        
+        ForEach(moviesResults, id: \.self) { movie in
+            NavigationLink(destination: MovieDetailView(content: movieViewModel.movieDetail), isActive: $movieViewModel.isMovieDetailActive) {
+                PopularCardView(movieResult: movie, isFavorite: getFavoriteItem(movie), genres: genres)
+                    .padding(.vertical, 10)
+                    .environmentObject(contentBindigs)
+                    .onTapGesture {
+                        movieViewModel.getMovieDetail(id: movie.id ?? -1)
+                    }
+            } //: LINK
+        } //: LOOP
     }
     
     private func getFavoriteItem(_ movieResult: MovieResult) -> Bool {
