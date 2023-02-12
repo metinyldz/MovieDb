258a6187d70326aae6d122a5bd9295d417cbc31e
metinyldz <metinyildiz2006@gmail.com>
2023-02-12 23:31:47 +0300

    Edited profileView-2

--- MovieDb/View/Profile/ProfileView.swift
+++ MovieDb/View/Profile/ProfileView.swift
@@ -28,62 +28,6 @@ struct ProfileView: View {
     }
 }
 
-struct ProfileViewList: View {
-    
-    @EnvironmentObject var contentBindigs: ContentBindigs
-    
-    var body: some View {
-        //MARK: - CENTER -
-        VStack {
-            Text("Favorite")
-                .font(Font.system(size: 22))
-                .fontWeight(.bold)
-        } //: VSTACK
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
-        .padding(.horizontal, 24)
-        .padding(.bottom, 10)
-        
-        //MARK: - FOOTER -
-        ForEach(contentBindigs.favoriteContents, id: \.self) { item in
-            ProfileFavoriteCardView(favoriteItem: item)
-                .padding(.vertical, 10)
-        }
-    }
-}
-
-struct ProfielViewHeader: View {
-    
-    var body: some View {
-        ZStack {
-            VStack {
-                Rectangle()
-                    .frame(height: 120.0)
-                    .frame(minWidth: 0, maxWidth: .infinity)
-                    .foregroundColor(Color("VibrantBlue"))
-                
-                Spacer()
-            } //: VStack
-            
-            VStack(alignment: .leading) {
-                Text("Hello \(Image("iconHandshake"))")
-                    .font(Font.system(size: 20))
-                    .fontWeight(.regular)
-                    .foregroundColor(Color.white)
-                    .frame(height: 30)
-                
-                Text("Jessie Doe")
-                    .font(Font.system(size: 25))
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.white)
-                    .frame(height: 30)
-            }
-            .frame(maxWidth: .infinity, maxHeight: 60, alignment: .leading)
-            .padding(.horizontal, 24)
-            .padding(.top, -30)
-        }
-    }
-}
-
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
