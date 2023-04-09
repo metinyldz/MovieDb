94e54fbce6893fdcf13aa386544c3a7f12cb9cf1
metinyldz <metinyildiz2006@gmail.com>
2023-04-09 18:47:14 +0300

    Fixed back button and image ratio bug

--- MovieDb/View/Movie/MovieDetailView.swift
+++ MovieDb/View/Movie/MovieDetailView.swift
@@ -8,29 +8,53 @@
 import SwiftUI
 
 struct MovieDetailView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var content: MovieDetailModel
     
     var body: some View {
-        ScrollView {
-            if content.genres != nil {
-                VStack {
-                    //MARK: - HEADER -
-                    MovieDetailHeaderView(posterPath: content.poster_path ?? "")
+        GeometryReader { geometry in
+            ScrollView {
+                if content.genres != nil {
+                    VStack {
+                        //MARK: - HEADER -
+                        GeometryReader { imageGeometry in
+                            AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500\(content.poster_path ?? "")")) { image in
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: geometry.size.width, height: max(400, imageGeometry.frame(in: .global).minY + 400))
+                                    .clipped()
+                                    .offset(y: -imageGeometry.frame(in: .global).minY)
+                            } placeholder: {
+                                Image("moviePlaceholder")
+                                    .resizable()
+                            }
+                        }
                         .frame(height: 400)
-                    
-                    //MARK: - CENTER -
-                    MovieDetailCenterView(content: content)
-                        .padding(.top, -20)
-                    
-                    //MARK: - FOOTER -
-                    MovieDetailFooterView(content: content)
-                        .padding(.horizontal, 24)
-                } //: VStack
-            }
-        } //: Scroll
-        .background(Color("BackgroundColor"))
-        .navigationBarHidden(true)
+                        
+                        //MARK: - CENTER -
+                        MovieDetailCenterView(content: content)
+                            .padding(.top, -20)
+                        
+                        //MARK: - FOOTER -
+                        MovieDetailFooterView(content: content)
+                            .padding(.horizontal, 24)
+                    } //: VStack
+                }
+            } //: Scroll
+            .background(Color("BackgroundColor"))
+            .navigationBarHidden(true)
+            .overlay(
+                Image(systemName: "arrow.left")
+                    .foregroundColor(Color.white)
+                    .frame(width: 25, height: 25)
+                    .onTapGesture {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }
+                    .padding(32)
+            , alignment: .topLeading)
+        }
     }
 }
 
@@ -61,23 +85,6 @@ struct MovieDetailHeaderView: View {
                             .frame(height: global.minY > 0 ? (400) + global.minY :  400)
                     }
                 }
-            } //: VStack
-            
-            VStack {
-                HStack {
-                    Image(systemName: "arrow.left")
-                        .foregroundColor(Color.white)
-                        .frame(width: 25, height: 25)
-                        .onTapGesture {
-                            self.presentationMode.wrappedValue.dismiss()
-                        }
-                        .padding(.horizontal, 24)
-                    
-                    Spacer()
-                } //: HStack
-                .frame(maxWidth: .infinity, maxHeight: 50)
-                
-                Spacer()
             } //: VStack
         } //: ZStack
     }
