2c70979f4f194132b0f08576d1bd61275e13264e
metinyldz <metinyildiz2006@gmail.com>
2023-05-28 22:32:02 +0300

    Added @MainActor

--- MovieDb/ViewModels/Movie/MovieViewModel.swift
+++ MovieDb/ViewModels/Movie/MovieViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+@MainActor
 class MovieViewModel: BaseViewModel {
     @Published var topRatedMovies: MovieTopRated = MovieTopRated()
     @Published var movies: Movie = Movie()
