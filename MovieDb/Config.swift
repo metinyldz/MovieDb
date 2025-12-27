f31b488ae029a7bb216587c13b7c9a7e8cd13ba4
Metin Yıldız <metinyildiz2006@gmail.com>
2025-12-27 21:27:13 +0300

    Added APIConfig file

---
+++ MovieDb/Config.swift
@@ -0,0 +1,12 @@
+//
+//  Config.swift
+//  MovieDb
+//
+//  Created by Metin Yıldız on 27.12.2025.
+//
+
+import Foundation
+
+struct APIConfig {
+    static let apiKey = "876d2569fd8e190ff8694b014b1b07b6"
+}
